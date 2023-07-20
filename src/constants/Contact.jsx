1f5102deff40f8d58bda6e3c8f2848074debc751
Htoo Khant <vexixongaming02@gmail.com>
2023-07-21 01:31:02 +0630

    h

--- src/constants/Contact.jsx
+++ src/constants/Contact.jsx
@@ -2,21 +2,21 @@ import { PiGearSixFill } from "react-icons/pi";
 import { BiLogoGmail } from "react-icons/bi";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 const Contact = [
-    {
-        id: 2,
-        icon: <FaGithubSquare />,
-        link: "",
-    },
-    {
-        id: 3,
-        icon: <BiLogoGmail />,
-        link: "",
-    },
-    {
-      id: 1,
-      icon: <FaLinkedin />,
-      link: "",
-    },
+  {
+    id: 2,
+    icon: <FaGithubSquare />,
+    link: "https://github.com/HtooKhant-vexix",
+  },
+  {
+    id: 3,
+    icon: <BiLogoGmail />,
+    link: "",
+  },
+  {
+    id: 1,
+    icon: <FaLinkedin />,
+    link: "",
+  },
 ];
 
 export default Contact;
