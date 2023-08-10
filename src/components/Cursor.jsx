d19361349ca0c65b52bea2b2fd8c9a3e7a453ba8
Htoo Khant <vexixongaming02@gmail.com>
2023-08-10 21:06:18 +0630

    update cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,67 +1,26 @@
-import React from "react";
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import pt from "../assets/pointer.png";
+import cstyle from "./cursor.module.css";
 
 const Cursor = () => {
-  const pointer = {
-    cursor: "../assets/pointer.png",
-  };
   const cs = useRef(null);
-  const cursorVisible = useRef(true);
-  const endX = useRef(window.innerWidth / 2);
-  const endY = useRef(window.innerHeight / 2);
-  const _x = useRef(0);
-  const _y = useRef(0);
-  const requestRef = useRef(null);
-  const toggleCursor = () => {
-    if (cursorVisible.current) {
-      cs.current.style.opacity = 1;
-    } else {
-      cs.current.style.opacity = 0;
-    }
-  };
 
-  const mouseEnterEvent = () => {
-    cursorVisible.current = true;
-    toggleCursor();
-  };
-  // const mouseLeaveEvent = () => {
-  //   cursorVisible.current = false;
-  //   toggleCursor();
-  // };
-  const mouseLeaveEvent = () => {
-    console.log("leave")
-  };
- 
   useEffect(() => {
-    document.addEventListener("mousemove", mouseMoveEvent);
-    document.addEventListener("mouseleave", mouseLeaveEvent);
-    document.addEventListener("mouseenter", mouseEnterEvent);
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      const mouseX = clientX - cs.current.clientWidth / 2;
+      const mouseY = clientY - cs.current.clientHeight / 2;
+      cs.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
     return () => {
-      document.removeEventListener("mousemove", mouseMoveEvent);
-      document.removeEventListener("mouseenter", mouseEnterEvent);
-      document.removeEventListener('mouseleave', mouseLeaveEvent);
-      cancelAnimationFrame(requestRef.current)
+      document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
-  const mouseMoveEvent = (e) => {
-    cursorVisible.current = true;
-    toggleCursor();
-    endX.current = e.pageX;
-    endY.current = e.pageY;
-    cs.current.style.top = endY.current - 20 + "px";
-    cs.current.style.left = endX.current - 20 + "px";
-  };
-  return (
-    <img
-      src={pt}
-      ref={cs}
-      alt=""
-      className="z-[99] opacity-0 h w-8 h-8  pointer-events-none absolute  overflow-hidden top-[50%] right-[50%] translate-x-[50%] translate-y-[50%] "
-    />
-  );
+  return <img src={pt} ref={cs} alt="" className={cstyle.cursor} />;
 };
 
 export default Cursor;
-
