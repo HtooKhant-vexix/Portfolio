465a72cd147e12216351aba1b5e9e7b7f74ceb05
Htoo Khant <vexixongaming02@gmail.com>
2023-08-29 15:22:31 +0630

    repair nav

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import NavRe from './NavRe';
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div>
+      <NavRe />
+      <div className="m-0 p-0">
+        <motion.div
+          whileInView={{ y: 1, opacity: 1 }}
+          initial={{ y: "-190px", opacity: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            ease: "easeOut",
+          }}
+          className="landBg pt-24 h-[300px] sm:h-[500px] flex justify-center items-center"
+        >
+          <div className="flex flex-col bg-primary shadow-primary/50 shadow-2xl bg-opacity-30 backdrop-blur-lg sm:py-16 px-10 py-5 sm:px-20 rounded-3xl items-center">
+            <div className="text-5xl sm:text-7xl text-[#fafafa] font-bold">
+              My Works
+            </div>
+            <div className=" text-[#fafafa] mt-7 sm:text-2xl font-semibold  text-center">
+              The followings are my practical and personal projects.
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
 }
 
 export default About
