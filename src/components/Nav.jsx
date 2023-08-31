6cbf3226c523b3c3cb4a8cac3f19ab07c2174a85
Htoo Khant <vexixongaming02@gmail.com>
2023-09-01 01:39:31 +0630

    git

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,36 +4,40 @@ import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import NavLinks from "../constants/Index";
 import { useState, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
 
 const Nav = () => {
-   const [show, setShow] = useState(true);
-   const [lastScrollY, setLastScrollY] = useState(0);
+  const [show, setShow] = useState(true);
+  const [chg, setChg] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-   const controlNavbar = () => {
-     if (typeof window !== "undefined") {
-       if (window.scrollY > lastScrollY) {
-         // if scroll down hide the navbar
-         setShow(false);
-       } else {
-         // if scroll up show the navbar
-         setShow(true);
-       }
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShow(false);
+      } else {
+        // if scroll up show the navbar
+        setShow(true);
+      }
 
-       // remember current page location to use in the next move
-       setLastScrollY(window.scrollY);
-     }
-   };
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
 
-   useEffect(() => {
-     if (typeof window !== "undefined") {
-       window.addEventListener("scroll", controlNavbar);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
 
-       // cleanup function
-       return () => {
-         window.removeEventListener("scroll", controlNavbar);
-       };
-     }
-   }, [lastScrollY]);
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+    
+  }, [lastScrollY]);
   return (
     <div
       className={`active ${
@@ -41,7 +45,7 @@ const Nav = () => {
       } delay-500 duration-500 fixed w-full z-50`}
     >
       <div className="w-full flex z-40 justify-center relative backdrop-blur-md bg-white bg-opacity-80 ">
-        <div className="container  flex items-center">
+        <div className="container relative flex items-center">
           <motion.div whileHover={{ scale: 1.1 }} className="ms-3">
             <img className="h-[70px]" src={logo} alt="" />
           </motion.div>
@@ -61,6 +65,33 @@ const Nav = () => {
               </NavLink>
             ))}
           </ul>
+          <div onClick={() => setChg(!chg)} className="sm:hidden ms-auto me-8">
+            {chg ? (
+              <MdClose className="text-4xl duration-100 text-primary" />
+            ) : (
+              <HiMenu className="text-4xl duration-100 text-primary" />
+            )}
+          </div>
+          <div className="w-[90%] mx-auto bottom-[-450px] inset-x-0 rounded-xl bg-primary/50 backdrop-blur-md absolute">
+            {chg && !show ? (
+              <ul className="list-none text-primary justify-end text-center items-center flex-col">
+                {NavLinks.map((e) => (
+                  <NavLink to={`/${e.name}`} key={e.id}>
+                    {/* <a href={`/${e.name}`} key={e.id}> */}
+                    <motion.li
+                      // whileHover={{ scale: 1.2 }}
+                      // key={e.id}
+                      className="hover:text-secondary cursor-none duration-75 items-center py-8 text-sm font-semibold flex px-6 mr-2 font-poppins"
+                    >
+                      <div className="flex me-2 text-2xl">{e.icon}</div>
+                      {e.label}
+                    </motion.li>
+                    {/* </a> */}
+                  </NavLink>
+                ))}
+              </ul>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
